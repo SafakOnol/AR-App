1e80f7966e56b4503021ee1182310b7060c78009
SafakOnol <onolsafak@outlook.com>
2022-10-31 23:53:00 -0400

    Add Unity listeners and methods

--- Assets/Scripts/ARManager.cs
+++ Assets/Scripts/ARManager.cs
@@ -11,4 +11,30 @@ public class ARManager : MonoBehaviour
     public ARPlaneManager planeManager;
     public GameObject asset;
 
+    // Unity Messages
+    private void OnEnable()
+    {
+        SetEvents();
+    }
+
+    private void OnDisable()
+    {
+        ClearEvents();
+    }
+
+    // Methods
+    private void SetEvents()
+    {
+        pointCloudManager.pointCloudsChanged += OnPointCloudsChanged;
+    }
+
+    private void ClearEvents()
+    {
+        pointCloudManager.pointCloudsChanged -= OnPointCloudsChanged;
+    }
+    private void OnPointCloudsChanged(ARPointCloudChangedEventArgs pEventArgs)
+    {
+        Debug.Log("Point Cloud Changed");
+    }
+
 }
