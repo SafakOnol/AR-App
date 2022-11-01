12e03ca3bc2288ecb7b9ddf684dfd11484a663b8
SafakOnol <onolsafak@outlook.com>
2022-10-31 23:41:16 -0400

    Add ARCore

---
+++ Assets/Scripts/ARManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARManager : MonoBehaviour
+{
+    // Editor Fields
+    public ARSessionOrigin sessionOrigin;
+    public ARPointCloudManager pointCloudManager;
+    public ARPlaneManager planeManager;
+    public GameObject asset;
+
+}
